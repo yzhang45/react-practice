1ea4e1e15eda0d5d1e75aa741a280945b9bb4213
yzhang45 <yzhang45@uvm.edu>
2020-02-14 15:45:37 -0500

    Create an app that shows a course table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+        <div className="Course">
+          <header className="Course-header">
+            <table>
+              <thead>
+              <tr>
+                <th>College</th>
+                <th>Department</th>
+                <th>Number</th>
+                <th>Section</th>
+                <th>Professor</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>CAS</td>
+                <td>CS</td>
+                <td>112</td>
+                <td>B1</td>
+                <td>Sullivan</td>
+              </tr>
 
+              <tr>
+                <td>CAS</td>
+                <td>CS</td>
+                <td>332</td>
+                <td>A1</td>
+                <td>Bun</td>
+              </tr>
+
+              <tr>
+                <td>CAS</td>
+                <td>CS</td>
+                <td>350</td>
+                <td>A1</td>
+                <td>Sarkar</td>
+              </tr>
+
+              </tbody>
+            </table>
+
+          </header>
+        </div>
+
+    );
+  }
+}
 export default App;
