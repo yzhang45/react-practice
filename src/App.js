eb22cac9b420048a128bf831956fbaf9289359dd
yzhang45 <yzhang45@uvm.edu>
2020-02-15 15:41:29 -0500

    updated code

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Course {
+  constructor(college, department, number, section, professor) {
+    this.college = college;
+    this.department = department;
+    this.number = number;
+    this.section = section;
+    this.professor = professor;
+    this.key = `${college} ${department} ${number} ${section}`;
+  }
+}
 
 class App extends Component {
   render() {
+    const columns = [
+      'College',
+      'Department',
+      'Number',
+      'Section',
+      'Professor',
+    ];
+    const courses = [
+      new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
+      new Course('CAS', 'CS', '332', 'A1', 'Bun'),
+      new Course('CAS', 'CS', '350', 'A1', 'Sarkar'),
+    ];
+
     return (
-        <div className="Course">
-          <header className="Course-header">
-            <table>
-              <thead>
-              <tr>
-                <th>College</th>
-                <th>Department</th>
-                <th>Number</th>
-                <th>Section</th>
-                <th>Professor</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>CAS</td>
-                <td>CS</td>
-                <td>112</td>
-                <td>B1</td>
-                <td>Sullivan</td>
-              </tr>
-
-              <tr>
-                <td>CAS</td>
-                <td>CS</td>
-                <td>332</td>
-                <td>A1</td>
-                <td>Bun</td>
-              </tr>
-
-              <tr>
-                <td>CAS</td>
-                <td>CS</td>
-                <td>350</td>
-                <td>A1</td>
-                <td>Sarkar</td>
-              </tr>
-
-              </tbody>
-            </table>
-
-          </header>
+        <div className="App">
+          <table>
+            <thead>
+            <tr>
+              {columns.map(column =>
+                  <th key={column}>{column}</th>)}
+            </tr>
+            </thead>
+            <tbody>
+            {
+              courses.map(course =>
+                  <tr key={course.key}>
+                    <td>{course.college}</td>
+                    <td>{course.department}</td>
+                    <td>{course.number}</td>
+                    <td>{course.section}</td>
+                    <td>{course.professor}</td>
+                  </tr>
+              )
+            }
+            </tbody>
+          </table>
         </div>
-
     );
   }
 }
+
 export default App;
